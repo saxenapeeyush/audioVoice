978432f1888906af04cfcc0cc42360c198b67ba8
Peeyush Saxena <saxena.piyush.011@gmail.com>
2019-07-30 14:46:43 +0530

    deleted apikey

--- assets/js/firebaseConfig.js
+++ assets/js/firebaseConfig.js
@@ -1,10 +1,10 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBOMBlsm_7j0qiLbDM7sQZLBF6tjqGH97I",
-    authDomain: "baintern.firebaseapp.com",
-    databaseURL: "https://baintern.firebaseio.com",
-    projectId: "baintern",
-    storageBucket: "baintern.appspot.com",
-    messagingSenderId: "713113054713",
-    appId: "1:713113054713:web:07b2a2bba524aacc"
+    apiKey: "put your api key here",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "app id"
   };
   firebase.initializeApp(firebaseConfig);
